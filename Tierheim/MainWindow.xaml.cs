4a34bddb3a8319054bc84941b9cd0d5ec9fd54ac
Thomas Steinfeld <>
2021-11-11 11:24:55 +0100

    Präsi

--- Tierheim/MainWindow.xaml.cs
+++ Tierheim/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,9 @@ namespace Tierheim
 		public MainWindow()
 		{
 			InitializeComponent();
+			if (Debugger.IsAttached)
+				this.Left = 2000;
+
 		}
 	}
 }
